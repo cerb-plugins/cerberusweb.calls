813b3b617bd304103d46121700c95d6a872b0197
Jeff Standen <jeff@webgroupmedia.com>
2018-07-26 13:56:28 -0700

    9.0 compatibility

--- api/uri/profiles/call.php
+++ api/uri/profiles/call.php
@@ -190,7 +190,6 @@ class PageSection_ProfilesCall extends Extension_PageSection {
 					//'worker_id' => $active_worker->id,
 					'total' => $total,
 					'return_url' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $url_writer->writeNoProxy('c=search&type=task', true),
-//					'toolbar_extension_id' => 'cerberusweb.explorer.toolbar.',
 				);
 				$models[] = $model;
 				
