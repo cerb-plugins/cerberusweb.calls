4cb082f54a3d837ac84d345f1bc61885c7be5501
Jeff Standen <jeff@webgroupmedia.com>
2018-08-07 17:03:18 -0700

    9.0.1 compatibility

--- api/uri/profiles/call.php
+++ api/uri/profiles/call.php
@@ -310,7 +310,9 @@ class PageSection_ProfilesCall extends Extension_PageSection {
 		
 		// If we have specific IDs, add a filter for those too
 		if(!empty($ids)) {
-			$view->addParam(new DevblocksSearchCriteria(SearchFields_CallEntry::ID, 'in', $ids));
+			$view->addParams([
+				new DevblocksSearchCriteria(SearchFields_CallEntry::ID, 'in', $ids)
+			], true);
 		}
 		
 		// Create batches
